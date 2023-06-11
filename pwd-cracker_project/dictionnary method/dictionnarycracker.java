acb71c1fef3e848ea79d14aaeaa36400e616383d
nakaty <nanakatyaidara@esp.sn>
2023-06-11 19:22:18 +0000

    my first commit

--- pwd-cracker_project/dictionnary method/dictionnarycracker.java
+++ pwd-cracker_project/dictionnary method/dictionnarycracker.java
@@ -6,31 +6,31 @@ import java.security.NoSuchAlgorithmException;
 
 public class dictionnarycracker {
     public static void main(String[] args) {
-        String passwordHash = "5baeb78464a76b6e118cb8ce32c5e8aece2d9531bd7e4075d6742ec042750e40";
+        String passwordHash = "59976e58055b7da234e2888214bdd2adae81ec081532d84e42e53ace7c2c30e2";
         String dictionaryFile = "passwords_hashes.txt";
-        
+
         String password = dictionaryAttack(passwordHash, dictionaryFile);
-        
+
         if (password != null) {
             System.out.println("Le mot de passe est : " + password);
         } else {
             System.out.println("Le mot de passe ne figure pas dans le dictionnaire.");
         }
     }
-    
+
     public static String dictionaryAttack(String passwordHash, String dictionaryFile) {
         try (BufferedReader br = new BufferedReader(new FileReader(dictionaryFile))) {
             String line;
             MessageDigest md = MessageDigest.getInstance("SHA-256");
-            
+
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(";");
                 String word = parts[0].trim();
-                
+
                 byte[] wordBytes = word.getBytes();
                 byte[] hashBytes = md.digest(wordBytes);
                 String computedHash = bytesToHexString(hashBytes);
-                
+
                 if (computedHash.equals(passwordHash)) {
                     return word;
                 }
@@ -38,17 +38,17 @@ public class dictionnarycracker {
         } catch (IOException | NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-        
+
         return null;
     }
-    
+
     public static String bytesToHexString(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
-        
+
         for (byte b : bytes) {
             sb.append(String.format("%02x", b));
         }
-        
+
         return sb.toString();
     }
 }
--- pwd-cracker_project/fabriqueMethodes.java
+++
@@ -1,6 +0,0 @@
-class FabriqueMethodes {
-    public static Methodes getInstance(String nom..) throws unknow
-        Methodes methodes = null;
-        int indexPoint = 
-        String 
-}
